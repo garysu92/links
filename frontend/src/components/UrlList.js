bcbdb51ff95f58d20da74dcaadc22a4a15724fd3
garysu92 <garysu929292@gmail.com>
2023-08-01 09:55:32 -0400

    .

--- frontend/src/components/UrlList.js
+++ frontend/src/components/UrlList.js
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 const UrlList = ({ urls }) => {
   return (
     <div>
+      If you do not see any links below, you may have to click <a href='https://lb-rqsu.onrender.com/api/urls'>here</a> and then come back.
       <ul>
         {urls.map((url) => (
           <li key={url._id}>
-            <a href={'//' + url.url} target="_blank" >{url.name}</a>
+            <a href={url.url} target="_blank" >{url.name}</a>
           </li>
         ))}
       </ul>
